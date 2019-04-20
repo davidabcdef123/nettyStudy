9edace60fcb7b2478e0e91b5fdffb783c77ef5c7
Super.sun <260681168@qq.com>
2019-04-20 23:31:41 +0800

    chat

--- nettyChat/src/main/java/com/david/chat/protocol/IMDecoder.java
+++ nettyChat/src/main/java/com/david/chat/protocol/IMDecoder.java
@@ -42,6 +42,7 @@ public class IMDecoder extends ByteToMessageDecoder {
             in.clear();
         } catch (Exception e) {
             e.printStackTrace();
+			ctx.channel().pipeline().remove(this);
         } finally {
         }
     }
